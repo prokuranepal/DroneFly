4ae03a20b8bca1624788fa001f5268a36d168844
Swainstha <swainstha@outlook.com>
2018-05-28 18:50:53 +0545

    status parsing done

--- app/src/main/java/com/example/swainstha/dronefly/MapsActivity.java
+++ app/src/main/java/com/example/swainstha/dronefly/MapsActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -11,9 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -42,6 +41,7 @@ import org.json.JSONObject;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
@@ -59,7 +59,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     ArrayList<StatusData> statusList;
     StatusListAdapter statusListAdapter;
 
+    boolean markerChanged = false;
 
+    JSONObject data;
     RelativeLayout relativeLayout;
     boolean showHide = false;
 
@@ -68,10 +70,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     Circle circle3;
     Polyline lineH;
     Polyline lineV;
+    Polyline dronePath;
     boolean circleUnCirle = false;
 
     Button show;
     Button circle;
+    Button fly;
+    Button downMission;
     EditText circleRadius;
 
     @Override
@@ -79,7 +84,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        //initSocket();
+        initSocket();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -133,9 +139,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             .center(latLng)
                             .radius(3 * radius).strokeWidth(1)
                             .strokeColor(Color.WHITE));
+
                     Geo geo = new Geo(latLng,3 * radius);
                     ArrayList<Double> list = geo.calculate();
                     Log.i("Info",list.toString());
+
                     lineH = mMap.addPolyline(new PolylineOptions()
                             .add(new LatLng(list.get(0), list.get(1)), new LatLng(list.get(4), list.get(5)))
                             .width(1)
@@ -155,7 +163,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     circleUnCirle = false;
                     circle.setText("CIRCLE");
                 }
+            }
+        });
 
+        fly = findViewById(R.id.fly);
+        fly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendCommand sendCommand = new SendCommand();
+                try {
+                    String res = sendCommand.execute("fly").get();
+                    Log.i("INFO", res);
+                } catch(ExecutionException e) {
+                    Log.i("INFO","Execution exception");
+                } catch(InterruptedException i) {
+                    Log.i("INFO","Interrupted exception");
+                }
+            }
+        });
+
+        downMission = findViewById(R.id.download_mission);
+        downMission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendCommand sendCommand = new SendCommand();
+                try {
+                    String res = sendCommand.execute("getMission","1").get();
+                    Log.i("INFO", res);
+                } catch(ExecutionException e) {
+                    Log.i("INFO","Execution exception");
+                } catch(InterruptedException i) {
+                    Log.i("INFO","Interrupted exception");
+                }
             }
         });
 
@@ -176,14 +215,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         statusList.add(new StatusData("Status: ","0"));
         statusList.add(new StatusData("LiDar: ","0"));
         statusList.add(new StatusData("Volt: ","0"));
+
         statusListAdapter = new StatusListAdapter(this,statusList);
         LayoutInflater inflater = getLayoutInflater();
         inflater.inflate(R.layout.status_list_view,statusListView, false);
 
         statusListView.setAdapter(statusListAdapter);
     }
-
-
 
 
 
@@ -230,7 +268,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             socket = IO.socket(urlString); //specifying the url
             socket.connect(); //connecting to the server
-            socket.emit("joinWebsite",Integer.toString(id) );  //specifying the join group to the server
+
+            SendCommand sendCommand = new SendCommand();
+            try {
+
+                //check for internet connectivity
+                if(!isOnline()) {
+                    throw new Exception();
+                }
+
+                //execute worker thread to send data
+                sendCommand.execute("joinAndroid").get();
+
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+                Log.i("INFO","Failed Sending");
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+                Log.i("INFO","Failed Sending");
+            }
 
             //callback functions for socket connected, message received and socket disconnected
 
@@ -247,6 +303,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                     final String res = args[0].toString();
                     Log.i("INFO",res);
+
+                    try {
+                        data = new JSONObject(res);
+                    } catch(JSONException e) {
+                        Log.i("INFO","Json Exception");
+                    }
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            try {
+
+                                for (int i = 0; i < statusList.size(); i++) {
+                                    statusList.get(i).setValue(data.names().get(i).toString());
+                                }
+
+                                statusListAdapter.notifyDataSetChanged();
+
+                                if(data.getString("arm").toString() == "true" && markerChanged) {
+                                    marker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.green));
+                                    markerChanged = false;
+                                } else if(data.getString("arm").toString() == "false" && !markerChanged){
+                                    marker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.red));
+                                    markerChanged = true;
+                                }
+                                marker.setRotation(Float.parseFloat(data.getString("head").toString()));
+
+                                marker.setPosition(new LatLng(Double.parseDouble(data.getString("lat").toString()),
+                                        Double.parseDouble(data.getString("lon").toString())));
+
+                                dronePath = mMap.addPolyline(new PolylineOptions()
+                                        .add(new LatLng(, ), new LatLng(, list.get(7)))
+                                        .width(2)
+                                        .color(Color.RED));
+
+                            } catch(JSONException e) {
+
+                                Log.i("INFO","Json exception in status data receive");
+
+                            }
+                        }
+                    });
+                    //Toast.makeText(getContext(), args[0].toString(), Toast.LENGTH_SHORT).show();
+                }
+
+            }).on("Mission", new Emitter.Listener() {
+
+                @Override
+                public void call(Object... args) {
+
+                    final String res = args[0].toString();
+                    Log.i("INFO",res);
                     try {
                         JSONObject data = new JSONObject(res);
                     } catch(JSONException e) {
@@ -255,12 +361,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     runOnUiThread(new Runnable() {
                         public void run() {
                             Log.i("INFO",res);
-                            marker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.red));
-                            marker.setRotation(90);
+
+
                         }
                     });
                     //Toast.makeText(getContext(), args[0].toString(), Toast.LENGTH_SHORT).show();
-
                 }
 
             }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
@@ -294,5 +399,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         return isConnected;
 
+    }
+
+    //sending data to the node js server so it can forward it, to the raspberry pi in drone to start the flight
+    public class SendCommand extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            try {
+                //sending message to server
+                socket.emit(urls[0]);
+                Log.i("INFO",urls[0]);
+                return "Success";
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.i("INFO","Failed Sending. May be no internet connection");
+                return "Failed";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            //Log.i("INFO",result);
+            result = "";
+        }
     }
 }
