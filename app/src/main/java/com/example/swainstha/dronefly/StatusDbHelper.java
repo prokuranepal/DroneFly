56d15e8649a49f95f7447009a6a23ec345ef90db
Swainstha <swainstha@outlook.com>
2018-06-03 12:55:09 +0545

    added sqlitedbhelper to add data to databse

--- app/src/main/java/com/example/swainstha/dronefly/StatusDbHelper.java
+++ app/src/main/java/com/example/swainstha/dronefly/StatusDbHelper.java
@@ -11,7 +11,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class StatusDbHelper extends SQLiteOpenHelper {
     // If you change the database schema, you must increment the database version.
     public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "FeedReader.db";
+    public static final String DATABASE_NAME = "StatusReader.db";
 
     private static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + StatusSave.StatusEntry.TABLE_NAME + " (" +
